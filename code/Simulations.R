343e4886c0aa891c08922831e5b9cb78da46314f
gus-pendleton <guspendleton@gmail.com>
2025-07-08 17:05:39 -0400

    Added supplementary

--- code/Simulations.R
+++ code/Simulations.R
@@ -6,9 +6,16 @@ library(patchwork)
 library(vegan)
 library(GUniFrac)
 
-source(plotting.R)
+source("code/plotting.R")
 
 nonsym_tree <- ape::read.tree("data/nonsym_tree.tree")
+sym_tree <- ape::read.tree("data/sym_tree.tree")
+
+sym_tree_plot <- ggtree(sym_tree) + 
+  geom_tiplab(label = paste("ASV_", c(1:4))) + 
+  geom_treescale(x = 0, width = 0.25) + 
+  scale_x_continuous(expand = expansion(c(0,.5))) + 
+  theme(plot.margin = unit(c(0,0,0, 0), "mm"))
 
 smol_tree_plot <- ggtree(nonsym_tree) + 
   geom_tiplab(label = paste("ASV_", c(1:4))) + 
@@ -24,14 +31,23 @@ flipped_tree_plot <- ggtree(nonsym_tree) +
 
 
 bf <- ggtree(nonsym_tree) + 
-  theme(plot.margin = unit(c(0,10,0, 0), "mm"))
+  theme(plot.margin = unit(c(0,5,0, 0), "mm"),
+        panel.background = element_rect(fill = "transparent",
+                                        color = "transparent"),
+        plot.background = element_rect(fill = "transparent",
+                                       color = "transparent"))
 br <- ggtree(nonsym_tree) + 
-  scale_x_reverse() + 
-  theme(plot.margin = unit(c(0,0,0, 10), "mm"))
+  scale_x_reverse() +
+  theme(plot.margin = unit(c(0,0,0, 5), "mm"),
+        panel.background = element_rect(fill = "transparent",
+                                        color = "transparent"),
+        plot.background = element_rect(fill = "transparent",
+                                       color = "transparent"))
 
 facing <- bf + br 
-ggsave(facing, filename = "figures/facing.png", width = 2, height = 2)
-ggsave(smol_tree_plot, filename = "figures/smol_tree_plot.png", width = 2, height = 2)
+ggsave(facing, filename = "figures/facing.pdf", width = 2, height = 1)
+ggsave(smol_tree_plot, filename = "figures/smol_tree_plot.pdf", width = 2, height = 2)
+ggsave(sym_tree_plot, filename = "figures/sym_tree_plot.pdf", width = 2, height = 2)
 ggsave(flipped_tree_plot, filename = "figures/flipped_tree_plot.png", width = 2, height = 2)
 
 possible_abunds <- expand.grid(rep(list(c(1,10,100)),4)) %>%
@@ -119,20 +135,50 @@ map(dists, \(x)broom::tidy(x)) %>%
          UR = Unifrac_Relative - Unifrac_Absolute) %>%
   pivot_longer(BR:UR, names_to = "Comparison", values_to = "Difference") %>%
   mutate(Comparison = factor(Comparison, levels = c("BR","UR", "BA"),
-                             labels = c("Rel.Bray - Abs.Uni",
-                                        "Rel.Uni - Abs.Uni",
-                                        "Abs.Bray - Abs.Uni"))) %>%
+                             labels = c(expression(italic(BC^{R}-U^{A})),
+                                        expression(italic(U^{R}-U^{A})),
+                                        expression(italic(BC^{A}-U^{A}))))) %>%
   ggplot(aes(x = Difference)) + 
-  geom_vline(xintercept = 0, linetype = 2) + 
+  geom_vline(xintercept = 0, linetype = 2, linewidth = .2) + 
   geom_histogram(bins = 50, alpha = 0.7, fill = "#6A3D9A") + 
-  facet_wrap(~Comparison) + 
+  facet_wrap(~Comparison, labeller = label_parsed) + 
   theme_classic() + 
   scale_y_continuous(expand = expansion(mult = 0)) + 
   scale_x_continuous(limits = c(-1,1)) + 
   labs(y = "Count", x = "Difference in Dissimilarity") + 
-  theme(strip.background = element_rect(color = "white"))
+  theme(strip.background = element_rect(color = "white"),
+        strip.text.x = element_text(size = 10))
 
-ggsave(filename = "figures/diff_hists.png", width = 6, height = 2)
+ggsave(filename = "figures/diff_hists.pdf", width = 6, height = 2)
+
+# Supplementary stuff:
+uni_abs_sym <- as.dist(GUniFrac(otu.tab = abs_abunds, tree = sym_tree, alpha = 1, normalize_counts = FALSE)$unifracs[, , "d_1"])
+
+dists <- list(Bray_Absolute = bray_abs,
+              Unifrac_Absolute = uni_abs_sym)
+
+map(dists, \(x)broom::tidy(x)) %>%
+  bind_rows(.id = "Distance") %>%
+  mutate(Comp_Key = paste(item1, item2, sep = ":")) %>%
+  select(-item1, -item2) %>%
+  pivot_wider(names_from = Distance, values_from = distance) %>%
+  rowwise() %>%
+  mutate( BA = Bray_Absolute - Unifrac_Absolute) %>%
+  pivot_longer(BA, names_to = "Comparison", values_to = "Difference") %>%
+  mutate(Comparison = factor(Comparison, levels = c("BA"),
+                             labels = c(expression(italic(BC^{A}-U^{A}))))) %>%
+  ggplot(aes(x = Difference)) + 
+  geom_vline(xintercept = 0, linetype = 2, linewidth = .2) + 
+  geom_histogram(bins = 50, alpha = 0.7, fill = "#6A3D9A") + 
+  facet_wrap(~Comparison, labeller = label_parsed) + 
+  theme_classic() + 
+  scale_y_continuous(expand = expansion(mult = 0)) + 
+  scale_x_continuous(limits = c(-1,1)) + 
+  labs(y = "Count", x = "Difference in Dissimilarity") + 
+  theme(strip.background = element_rect(color = "white"),
+        strip.text.x = element_text(size = 10))
+
+ggsave(filename = "figures/sym_hists.pdf", width = 3, height = 3)
 
 # Using real data
 
@@ -168,7 +214,9 @@ ordinate_and_plot <- function(dist, physeq, title){
          color = "Group") + 
     scale_color_manual(values = comp_group_colors_hier)+ 
     geom_point(alpha = 0.5, size = 4) + 
-    theme(axis.title.x = element_text(face = "bold"))
+    theme(axis.title = element_text(size = 20),
+          axis.title.x = element_text(face = "bold"),
+          plot.title = element_text(hjust = 0.5, size = 25))
 }
 
 ont_dists <- list(Rel_0 = rel_unifrac_dist_0,
@@ -178,23 +226,26 @@ ont_dists <- list(Rel_0 = rel_unifrac_dist_0,
                   Abs_5 = abs_unifrac_dist_0.5,
                   Abs_1 = abs_unifrac_dist)
 
-plots <- map2(ont_dists, c("alpha = 0.0",
-                           "alpha = 0.5",
-                           "alpha = 1.0",
-                           "alpha = 0.0",
-                           "alpha = 0.5",
-                           "alpha = 1.0"),
+plots <- map2(ont_dists, c("\u03b1 = 0.0",
+                           "\u03b1 = 0.5",
+                           "\u03b1 = 1.0",
+                           "\u03b1 = 0.0",
+                           "\u03b1 = 0.5",
+                           "\u03b1 = 1.0"),
               \(x,y)ordinate_and_plot(dist = x, physeq = full_abs_physeq, title = y))
 
 
 
-wrap_plots(plots[4:6]) + 
+wrap_plots(list(plots[[4]] + scale_x_reverse(), plots[[5]], plots[[6]])) + 
   plot_layout(ncol = 3, guides = "collect")
 
-ggsave(filename = "figures/ont_ordinations.png",width = 14, height = 5)
+ggsave(filename = "figures/ont_ordinations.png",width = 14, height = 5, dpi = 600)
 
 
+wrap_plots(plots[1:3]) + 
+  plot_layout(ncol = 3, guides = "collect")
 
+ggsave(filename = "figures/ont_ordinations_rel.png",width = 14, height = 5, dpi = 600)
 
 cell_dist <- sample_data(full_abs_physeq) %>%
   data.frame() %>%
@@ -219,20 +270,19 @@ ont_mantels %>%
   separate_wider_delim(Metric, delim = "_", names = c("Normalization", "Alpha")) %>%
   mutate(Alpha = as.numeric(Alpha)) %>%
   ggplot(aes(x = Alpha, y = Correlation, color = Normalization)) + 
-  geom_line() + 
   annotate(geom = "segment", y = ont_mantels$Bray_Rel, yend = ont_mantels$Bray_Rel,
-           x = 0, xend = 1, linetype = 2, color = "#196689") + 
+           x = 0, xend = 1, linetype = 2, color = "#196689", linewidth = 2) + 
   annotate(geom = "segment", y = ont_mantels$Bray_Abs, yend = ont_mantels$Bray_Abs,
-           x = 0, xend = 1, linetype = 2, color = "#B6AA0D") +  
-  labs(x = "Generalized alpha value", y = "Correlation with cell counts") + 
+           x = 0, xend = 1, linetype = 2, color = "#B6AA0D", linewidth = 2) +  
+  labs(x = "\u03b1", y = expression(Mantel~italic(r)~"(dist. vs. cells)")) + 
+  geom_line(linewidth = 2) + 
   scale_x_continuous(expand = expansion(mult = c(0, 0.05)))+ 
-  scale_color_manual(labels = c("Absolute Unifrac", "Relative Unifrac"),
-                     name = "",
-                     values = c("#F2C621", "#008FF8"))+ 
-  theme(legend.position = "none")
+  scale_color_manual(values = c("#F2C621", "#008FF8"))+ 
+  theme(legend.position = "none",
+        axis.title = element_text(size = 20))
 
 
-ggsave(filename = "figures/ont_mantel.png", width = 4, height = 3.5)
+ggsave(filename = "figures/ont_mantel.png", width = 4, height = 3.5, dpi=600)
 
 # Permanovas
 sam_data_for_adonis <- data.frame(sample_data(full_abs_physeq))
@@ -250,19 +300,18 @@ ont_perms %>%
   mutate(Alpha = as.numeric(Alpha)) %>%
   filter(term == "Comp_Group_Hier") %>%
   ggplot(aes(x = Alpha, y = R2, color = Normalization)) + 
-  geom_line() + 
   annotate(geom = "segment", y = ont_perms$Bray_Rel[[1,4]], yend = ont_perms$Bray_Rel[[1,4]],
-           x = 0, xend = 1, linetype = 2, color = "#196689") + 
+           x = 0, xend = 1, linetype = 2, color = "#196689", linewidth = 2) + 
   annotate(geom = "segment", y = ont_perms$Bray_Abs[[1,4]], yend = ont_perms$Bray_Abs[[1,4]],
-           x = 0, xend =1, linetype = 2, color = "#B6AA0D") +
-  labs(x = "Generalized alpha value", y = "R-Squared (Depth-Month Groups)") + 
+           x = 0, xend =1, linetype = 2, color = "#B6AA0D", linewidth = 2) +
+  geom_line(linewidth = 2) + 
+  labs(x = "\u03b1", y = expression(italic(R^2))) +
   scale_x_continuous(expand = expansion(mult = c(0,0.05)))+ 
-  scale_color_manual(labels = c("Absolute Unifrac", "Relative Unifrac"),
-                     name = "",
-                     values = c("#F2C621", "#008FF8"))+ 
-  theme(legend.position = "none")
+  scale_color_manual(values = c("#F2C621", "#008FF8"))+ 
+  theme(legend.position = "none",
+        axis.title = element_text(size = 20))
 
-ggsave(filename = "figures/ont_rsqr.png", width = 4, height = 3.5)
+ggsave(filename = "figures/ont_rsqr.png", width = 4, height = 3.5, dpi=600)
 
 ont_perms %>%
   bind_rows(.id = "Metric") %>%
@@ -271,18 +320,18 @@ ont_perms %>%
   mutate(Alpha = as.numeric(Alpha)) %>%
   filter(term == "Comp_Group_Hier") %>%
   ggplot(aes(x = Alpha, y = statistic, color = Normalization)) + 
-  geom_line() + 
   annotate(geom = "segment", y = ont_perms$Bray_Rel[[1,5]], yend = ont_perms$Bray_Rel[[1,5]],
-           x = 0, xend = 1, linetype = 2, color = "#196689") + 
+           x = 0, xend = 1, linetype = 2, color = "#196689", linewidth = 2) + 
   annotate(geom = "segment", y = ont_perms$Bray_Abs[[1,5]], yend = ont_perms$Bray_Abs[[1,5]],
-           x = 0, xend =1, linetype = 2, color = "#B6AA0D") +
-  labs(x = "Generalized alpha value", y = "F-Statistic (Depth-Month Groups)") + 
+           x = 0, xend =1, linetype = 2, color = "#B6AA0D", linewidth = 2) +
+  geom_line(linewidth = 2) + 
+  labs(x = "\u03b1", y = expression(italic(F)-statistic)) +
   scale_x_continuous(expand = expansion(mult = c(0,0.05)))+ 
-  scale_color_manual(labels = c("Absolute Unifrac", "Relative Unifrac"),
-                     name = "",
-                     values = c("#F2C621", "#008FF8"))+ 
-  theme(legend.position = "none")
+  scale_color_manual(values = c("#F2C621", "#008FF8"))+ 
+  theme(legend.position = "none",
+        axis.title = element_text(size = 20))
 
-ggsave(filename = "figures/ont_fstat.png", width = 4, height = 3.5)
+ggsave(filename = "figures/ont_fstat.png", width = 4, height = 3.5, dpi=600)
 
+## Permute absolute abundance of samples? 
 
